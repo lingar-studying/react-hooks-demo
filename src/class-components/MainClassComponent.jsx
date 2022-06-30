864f926ef446cddf546bed5d74d7f6061bf66787
lingar <yimprogramming@gmail.com>
2022-06-30 14:41:38 +0300

    Doing link to the homepage

--- src/class-components/MainClassComponent.jsx
+++ src/class-components/MainClassComponent.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import {Counter} from "./ManyClassComponents";
+import {Link} from "react-router-dom";
 
 export class MainClassComponent extends React.Component{
 
     render(){
         return(
             <div>
+                <Link to="/">return</Link>
+
                 <h1>Main Class Components</h1>
                 <Counter/>
 
--- src/hooks-components/MainHooksComponent.jsx
+++ src/hooks-components/MainHooksComponent.jsx
@@ -1,9 +1,14 @@
 // import React from "react";//Not needed in hooks
 
+import {Link} from "react-router-dom";
+import React from "react";
+
 export function MainHooksComponent(){//mainHooksComponent - won't work
 
     return(
         <div>
+            <Link to="/">return</Link>
+
             <h1>Main Hooks Component</h1>
         </div>
     );
